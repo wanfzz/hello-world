8a8ba261ace2afab9dd074e2f2446b1b6e53f679
wanfzz <wanf233@gmail.com>
2021-02-01 19:11:13 +0800

    11

--- project/nest + react + mysql/src/App.tsx
+++ project/nest + react + mysql/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { renderRoutes } from 'react-router-config'
+import { HashRouter, Redirect } from 'react-router-dom'
+import Home from '@/container/Home';
+import Login from '@/container/Login';
+
+const routerConfig = [
+  {
+    path: '/',
+    exact: true,
+    render: () => <Redirect to={"/home"} />
+  },
+  {
+    path: '/home',
+    component: Home,
+  },
+  {
+    path: '/login',
+    component: Login,
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        {renderRoutes(routerConfig)}
+      </HashRouter>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ project/nest + react + mysql/src/container/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+
+  return (
+     <div>
+       <input type="text" name="" id=""/>
+       <input type="password" name="" id=""/>
+       <button>LOGIN</button>
+     </div>
+  )
+}
+
+export default Login
